2acd0815a0b431492f41dffb78ff35498d72e0ae
David172082 <david172082@gmail.com>
2025-10-02 18:54:00 -0500

    Centrar columnas espaciado

--- app/src/main/java/com/example/cupcakeapp/StartOrderScreen.kt
+++ app/src/main/java/com/example/cupcakeapp/StartOrderScreen.kt
@@ -1,17 +1,22 @@
 package com.example.cupcakeapp
 
-
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun StartOrderScreenBasic(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.fillMaxSize().padding(16.dp)) {
-        Text("Start Order - basic layout")
+fun StartOrderScreenCentered(modifier: Modifier = Modifier, onNext: ()->Unit = {}) {
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Start your order")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = onNext) { Text("Next") }
     }
 }
