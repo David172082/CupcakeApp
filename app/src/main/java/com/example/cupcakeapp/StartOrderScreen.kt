17ddb73944a75539c39dea4f2eb402b8ca332931
David172082 <david172082@gmail.com>
2025-10-02 18:52:00 -0500

    Layout basico column

--- app/src/main/java/com/example/cupcakeapp/StartOrderScreen.kt
+++ app/src/main/java/com/example/cupcakeapp/StartOrderScreen.kt
@@ -1,5 +1,6 @@
 package com.example.cupcakeapp
 
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
