0995785e6adde7ef4eb5297959da1cb6cd1ad96d
David172082 <david172082@gmail.com>
2025-10-02 18:41:44 -0500

    Configurar entrypoint

---
+++ app/src/main/java/com/example/cupcakeapp/CupcakeApp.kt
@@ -0,0 +1,13 @@
+package com.example.cupcakeapp
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+
+@Composable
+fun CupcakeApp() {
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Text("Cupcake App - placeholder")
+    }
+}
