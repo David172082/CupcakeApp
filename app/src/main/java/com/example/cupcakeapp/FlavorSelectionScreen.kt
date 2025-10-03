f9b1e52f385eea885564af3c6dd887e90553f77f
David172082 <david172082@gmail.com>
2025-10-02 19:17:00 -0500

    Sabores subtotal

---
+++ app/src/main/java/com/example/cupcakeapp/FlavorSelectionScreen.kt
@@ -0,0 +1,7 @@
+package com.example.cupcakeapp
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FlavorSelectionScreen() { Text("Flavor selection (subtotal will update here)") }
